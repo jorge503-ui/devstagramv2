ddc6b2d64eb88d150e02b44ca649f897b9780f32
Jorge Peña <jorge.p.391@gmail.com>
2022-06-17 00:00:36 -0600

    Mostrar publicaciones en dashboard

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,8 +14,12 @@ class PostController extends Controller
     }
 
     public function index(User $user){
+
+        $posts = Post::where('user_id', $user->id)->paginate(20);
+
         return view('dashboard',[
-            'user'=>$user
+            'user'=>$user,
+            'posts' => $posts
         ]);
     }
 
