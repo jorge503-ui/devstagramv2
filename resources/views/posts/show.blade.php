2d819e77ebec6065c40366a26eb821067494937a
Jorge Peña <jorge.p.391@gmail.com>
2022-06-20 11:15:10 -0600

    Agregar comentario a publicaciones

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex">
+    <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{$post -> titulo}}">
             <div class="p-3">
@@ -23,8 +23,62 @@
             </div>
         </div>
 
-        <div class="md:w-1/2">
+        <div class="md:w-1/2 p-5">
+            <div class="shadow mb-5 bg-white p-5">
+                @auth
+                    <p class="text-xl font-bold text-center mb-4">
+                        Agrega un Nuevo Comentario
+                    </p>
+                    @if (session('mensaje'))
+                        <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold">
+                            {{session('mensaje')}}
+                        </div>
+                    @endif
+                    <form action="{{ route('comentarios.store', ['user'=>$user,'post'=>$post]) }}" method="POST">
+                        @csrf
+                        <div class="mb-5">
+                            <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
+                                Añade un comentario
+                            </label>
+                            <textarea id="comentario"
+                                    name="comentario"
+                                    type="text"
+                                    placeholder="Agrega un comentario"
+                                    class="border p-3 w-full rounded-lg @error('comentario')
+                                        border-red-500
+                                    @enderror"
+                                    >{{old('comentario')}}</textarea>
 
+                            @error('comentario')
+                                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                                    {{$message}}
+                                </p>
+                            @enderror
+                        </div>
+
+                        <input
+                            type="submit"
+                            value="Comentar"
+                            class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer
+                            uppercase font-bold w-full p-3 text-white rounded-lg"
+                        >
+                    </form>
+                @endauth
+
+                <div class="bg-white shadow mb-5 max-h-96 overflow-y-scroll mt-10">
+                    @if ($post->comentarios->count())
+                        @foreach ($post->comentarios as $comentario )
+                            <div class="p-5 border-gray-300 border-b">
+                                <a href="{{ route('posts.index', ['user'=>$comentario->user]) }}" class="font-bold">{{$comentario->user->username}}</a>
+                                <p>{{$comentario->comentario}}</p>
+                                <p class="text-sm text-gray-500">{{$comentario->created_at->diffForHumans()}}</p>
+                            </div>
+                        @endforeach
+                    @else
+                        <p class="p-10 text-center">No hay comentarios aún</p>
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ComentarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,5 +34,8 @@ Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.in
 Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
 Route::post('/posts',[PostController::class,'store'])->name('posts.store');
 Route::get('/{user:username}/posts/{post}',[PostController::class,'show'])->name('posts.show');
+
+Route::post('/{user:username}/posts/{post}',[ComentarioController::class,'store'])->name('comentarios.store');
+
 
 Route::post('/imagenes',[ImagenController::class,'store'])->name('imagenes.store');
