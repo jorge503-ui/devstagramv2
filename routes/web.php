eaaa0fb806ea367673f6683e6e371a213b42f526
Jorge Peña <jorge.p.391@gmail.com>
2022-06-20 12:01:24 -0600

    Eliminacion de publicacion

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,8 @@ Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.in
 Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
 Route::post('/posts',[PostController::class,'store'])->name('posts.store');
 Route::get('/{user:username}/posts/{post}',[PostController::class,'show'])->name('posts.show');
+Route::delete('posts/{post}',[PostController::class,'destroy'])->name('posts.destroy');
 
 Route::post('/{user:username}/posts/{post}',[ComentarioController::class,'store'])->name('comentarios.store');
-
 
 Route::post('/imagenes',[ImagenController::class,'store'])->name('imagenes.store');
