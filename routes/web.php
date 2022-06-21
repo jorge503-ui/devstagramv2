5706da99b0bff216a501ca4a70e2728b615c3d84
Jorge Peña <jorge.p.391@gmail.com>
2022-06-20 23:38:10 -0600

    Funcionalidad de likes a posts

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\LikeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,3 +40,7 @@ Route::delete('posts/{post}',[PostController::class,'destroy'])->name('posts.des
 Route::post('/{user:username}/posts/{post}',[ComentarioController::class,'store'])->name('comentarios.store');
 
 Route::post('/imagenes',[ImagenController::class,'store'])->name('imagenes.store');
+
+//Like a las fotos
+Route::post('/posts/{post}/likes',[LikeController::class,'store'])->name('posts.likes.store');
+Route::delete('/posts/{post}/likes',[LikeController::class,'destroy'])->name('posts.likes.destroy');
